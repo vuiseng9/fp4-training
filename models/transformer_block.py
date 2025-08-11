976c07961b587ad0c48a50a8864ea09036968ad5
Vui Seng Chua <vuiseng9@gmail.com>
2025-08-11 22:23:53 +0000

    Minor patch

--- models/transformer_block.py
+++ models/transformer_block.py
@@ -5,13 +5,13 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from custom.linear import CustomLinear
 
 try:
     import transformer_engine.pytorch as te
 except ImportError:
     Warning("transformer_engine.pytorch is not installed.")
-
+    te = None
+    
 LINEAR_IMPL = {
     "torch": nn.Linear,
     "te": te.Linear if te is not None else None, # dont fall back, so that we are aware what is going on
