84c697a91797798326b3291f5556f776d0310629
Vui Seng Chua <vuiseng9@gmail.com>
2025-09-21 03:46:56 +0000

    change cuda arch custom/setup.py

--- custom/setup.py
+++ custom/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 import os
-os.environ["TORCH_CUDA_ARCH_LIST"] = "12.0"
+os.environ["TORCH_CUDA_ARCH_LIST"] = "10.0;12.0"
 # Debug flags for C++
 cpp_debug_flags = [
     '-g',           # Generate debug symbols
