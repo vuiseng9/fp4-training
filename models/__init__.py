6efbfb1f5cd99b34e4f49bdd447967b8736a994b
Vui Seng Chua <vuiseng9@gmail.com>
2025-10-13 18:48:19 +0000

    Add usage of te.LayerNormLinear for TinyViT model

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .transformer_block import TransformerBlock, LINEAR_IMPL
 from .vit import TinyViT
+from .fused_norm_vit import FusedNormTinyViT
 
-__all__ = ["TransformerBlock", "TinyViT", "LINEAR_IMPL"]
+__all__ = ["TransformerBlock", "TinyViT", "LINEAR_IMPL", "FusedNormTinyViT"]
--- models/transformer_block.py
+++ models/transformer_block.py
@@ -65,6 +65,35 @@ class TransformerBlock(nn.Module):
         return residual + hidden
     
 
+class FusedNormTransformerBlock(nn.Module):
+    def __init__(self, E, F, H, dropout=0.1, impl="LayerNorm"):
+        super().__init__()
+        self.E = E
+        self.F = F
+        self.H = H
+        self.impl = impl
+
+        self.attn = FusedNormAttentionBlock(E, H, dropout, impl=impl)
+
+        self.ffn = nn.ModuleDict({
+            "norm_up_proj": te.LayerNormLinear(E, F, normalization=impl),
+            "act": nn.GELU(),
+            "act_dropout": nn.Dropout(dropout),
+            "down_proj": te.Linear(F, E),
+            "dropout": nn.Dropout(dropout),
+        })
+
+    def forward(self, x):
+        # x in (B, L, E)
+
+        hidden = self.attn(x)
+        residual = hidden
+
+        for _, layer in self.ffn.items():
+            hidden = layer(hidden)
+
+        return residual + hidden
+
 class AttentionBlock(nn.Module):
     def __init__(self, E, H, dropout=0.1, impl="torch"):
         super().__init__()
@@ -106,7 +135,49 @@ class AttentionBlock(nn.Module):
         out = self.o_proj(attn)
 
         return residual + out
-    
+
+
+class FusedNormAttentionBlock(nn.Module):
+    def __init__(self, E, H, dropout=0.1, impl="LayerNorm"):
+        super().__init__()
+        assert E % H == 0, "head size is not multiple of embedding size"
+
+        self.E = E
+        self.H = H
+        self.dh = E//H
+        self.attn_scale = 1 / (self.dh ** -0.5)
+
+        self.norm_qkv_proj = te.LayerNormLinear(E, 3 * E, normalization=impl)
+        self.o_proj = te.Linear(E, E)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # Image shape transformation
+        # B, IC, H, W
+        # B, OC(E), H, W
+        B, L, E = x.shape
+        residual = x
+        
+        x   = self.norm_qkv_proj(x).split(E, dim=-1)
+
+        q   = x[0].view(B, L, self.H, self.dh).transpose(1, 2)                   # B, H, L, dh
+        k_t = x[1].view(B, L, self.H, self.dh).transpose(1, 2).transpose(-2, -1) # B, H, dh, L
+        v   = x[2].view(B, L, self.H, self.dh).transpose(1, 2)                   # B, H, L, dh
+
+        score = (q @ k_t) * self.attn_scale
+        attn = F.softmax(score, dim=-1)
+        prob = self.dropout(attn)
+
+        attn = prob @ v
+
+        # attn (B, H, L, dh) -> (B, L, H, dh) -> (B, L, E) 
+        attn = attn.transpose(1, 2).contiguous().view(B, L, E)
+
+        out = self.o_proj(attn)
+
+        return residual + out
+
+
 if __name__ == "__main__":
     emb_dim = 64
     expansion_dim = emb_dim*2
